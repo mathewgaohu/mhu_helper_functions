a44fbe7b63fc94d1ac1fab0a404f5cc386c96aa9
mathewgaohu <mathewgaohu@gmail.com>
2024-08-18 14:31:34 -0500

    Add documnet, reference, and modify formula for accuracy

--- mhu_helper_functions/matrix_function_lanczos.py
+++ mhu_helper_functions/matrix_function_lanczos.py
@@ -1,3 +1,9 @@
+"""Lanczos method for symmetric matrix function approximation. 
+
+Reference:
+Musco, Cameron, Christopher Musco, and Aaron Sidford. "Stability of the Lanczos method for matrix function approximation." In Proceedings of the Twenty-Ninth Annual ACM-SIAM Symposium on Discrete Algorithms, pp. 1605-1624. Society for Industrial and Applied Mathematics, 2018.
+"""
+
 from typing import Mapping
 
 import numpy as np
@@ -13,6 +19,14 @@ class FunctionLinearOperator(spla.LinearOperator):
         fun: Mapping,
         rank: int,
     ):
+        """Create the Matrix Function f(A) as a Linear Operator.
+
+        Args:
+            A (spla.LinearOperator): The symmetric Matrix in Linear Operator format.
+            fun (Mapping): The function handle.
+            rank (int): Number of Lanczos iterations
+        """
+        A = spla.aslinearoperator(A)
         assert A.shape[0] == A.shape[1]
         super().__init__(dtype=A.dtype, shape=A.shape)
         self.A = A
@@ -41,12 +55,22 @@ class FunctionLinearOperator(spla.LinearOperator):
 
         evals_T, evecs_T = sla.eigh_tridiagonal(alphas, betas[1:-1])
         f_of_T = (evecs_T * self.fun(evals_T)) @ evecs_T.T
-        return Q @ (f_of_T @ (Q.T @ x))
+        # return Q @ (f_of_T @ (Q.T @ x)) # less accurate
+        return np.linalg.norm(x) * (Q @ (f_of_T[:, 0]))
 
     def _rmatvec(self, x):
         return self._matvec(x)
 
 
-def InvSqrtLinearOperator(A: spla.LinearOperator, rank: int):
+def InvSqrtLinearOperator(A: spla.LinearOperator, rank: int) -> spla.LinearOperator:
+    """Create the sqrt inverse A^{-1/2} as a Linear Operator.
+
+    Args:
+        A (spla.LinearOperator): The symmetric Matrix in Linear Operator format.
+        rank (int): Number of Lanczos iterations
+
+    Returns:
+        spla.LinearOperator: A^{-1/2}
+    """
     fun = lambda x: 1.0 / np.sqrt(x)
     return FunctionLinearOperator(A, fun, rank)
