364dbf9bf9d1818cb2206caaf4d1d9ad005a9e26
mathewgaohu <mathewgaohu@gmail.com>
2025-06-02 00:53:27 -0500

    cleaner implementation of cleaning checkpoints

--- mhu_helper_functions/clean_checkpoints.py
+++ mhu_helper_functions/clean_checkpoints.py
@@ -5,35 +5,20 @@ from pathlib import Path
 import numpy as np
 
 
+def stack_vectors(folder_path: str, name: str):
+    """Stack all name*.npy under the path. Then remove singe files."""
+    path = Path(folder_path)
+    N = len([p for p in path.glob(f"{name}*.npy") if p.stem[len(name) :].isdigit()])
+    print(f"Found {N} {name} numpy files.")
+    if N > 0:
+        xx = np.stack([np.load(path / f"{name}{i}.npy") for i in range(N)])
+        np.save(path / f"{name}{name}.npy", xx)
+    for i in range(N):
+        (path / f"{name}{i}.npy").unlink()
+
+
 def clean_map(output_dir: str):
-    """Clean results of iterative solvers (lbfgs and newton).
-
-    1. Concatenate all checkpoints x, f, g, and p.
-    Note that the number of x is often one more than the number of p (when converged).
-    Their number are same if line search fails (not a good result)
-    """
-    path = Path(output_dir)
-    save = lambda x, name: np.save(path / name, x)
-    load = lambda name: np.load(path / f"{name}.npy")
-
-    N = len([p for p in path.glob("x*.npy") if p.stem[1:].isdigit()])
-    print(f"Found {N} x arrays.")
-    if N > 0:
-        xx = np.asarray([load(f"x{i}") for i in range(N)])
-        ff = np.asarray([load(f"f{i}") for i in range(N)])
-        gg = np.asarray([load(f"g{i}") for i in range(N)])
-        save(xx, "xx")
-        save(ff, "ff")
-        save(gg, "gg")
-    for i in range(N):
-        (path / f"x{i}.npy").unlink()
-        (path / f"f{i}.npy").unlink()
-        (path / f"g{i}.npy").unlink()
-
-    N = len([p for p in path.glob("p*.npy") if p.stem[1:].isdigit()])
-    print(f"Found {N} p arrays.")
-    if N > 0:
-        pp = np.asarray([load(f"p{i}") for i in range(N)])
-        save(pp, "pp")
-    for i in range(N):
-        (path / f"p{i}.npy").unlink()
+    stack_vectors(output_dir, "x")
+    stack_vectors(output_dir, "f")
+    stack_vectors(output_dir, "g")
+    stack_vectors(output_dir, "p")
